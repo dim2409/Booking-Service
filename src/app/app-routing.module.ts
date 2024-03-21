bf08922aaf98ec86b0b71dc0c5c65ae666154e79
Ioannis Tsilikas <tsilikas@arx.gr>
2024-03-21 12:20:56 +0200

    add room management route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './routes/home/home.component';
 import { MyBookingsComponent } from './routes/my-bookings/my-bookings.component';
 import { BookingRequestComponent } from './routes/booking-request/booking-request.component';
 import { ModeratorDashboardComponent } from './routes/moderator-dashboard/moderator-dashboard.component';
+import { RoomManagementComponent } from './routes/room-management/room-management.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'myBookings', component: MyBookingsComponent},
   { path: 'makeABooking', component: BookingRequestComponent},
   { path: 'moderator', component: ModeratorDashboardComponent},
+  { path: 'roomManagement', component: RoomManagementComponent},
 ];
 
 @NgModule({
---
+++ src/app/routes/room-management/room-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './room-management.component.html',
+  styleUrl: './room-management.component.less'
+})
+export class RoomManagementComponent {
+
+}
