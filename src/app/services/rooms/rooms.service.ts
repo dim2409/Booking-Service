3878c9c0e881e74b10649027b2b6206144b476f1
Ioannis Tsilikas <tsilikas@arx.gr>
2024-04-17 20:23:23 +0300

    service adjustement

--- src/app/services/rooms/rooms.service.ts
+++ src/app/services/rooms/rooms.service.ts
@@ -17,11 +17,11 @@ export class RoomsService {
     return this.http.post(environment.apiUrl + '/getDepartments', req);
   }
   getBuildings(req: any): Observable <any> {
-    return this.http.post(environment.apiUrl + '/getBuildings', req);
+    return this.http.post('/api/getBuildings', req);
   }
 
   getAllRooms(): Observable <any> {
-    return this.http.get(environment.apiUrl + '/getAllRooms');
+    return this.http.get('api/getAllRooms');
   }
 
   getModeratedRooms(id: number): Observable <any> {
