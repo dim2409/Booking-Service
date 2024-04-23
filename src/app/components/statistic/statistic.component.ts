cddf5fb0f6787c9e378465779d93a6535e8de064
Ioannis Tsilikas <tsilikas@arx.gr>
2024-04-23 22:53:17 +0300

    style adjust

--- src/app/components/statistic/statistic.component.ts
+++ src/app/components/statistic/statistic.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDateRangeInput, MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelect, MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 import { ChartConfiguration, ChartData } from 'chart.js';
 import { tr } from 'date-fns/locale';
 import { BaseChartDirective } from 'ng2-charts';
@@ -29,7 +33,10 @@ import { StatisticsService } from 'src/app/services/statistics/statistics.servic
     MatDatepickerModule,
     ReactiveFormsModule,
     MatChipsModule,
-    MatDateRangeInput
+    MatDateRangeInput,
+    MatButtonModule,
+    MatMenuModule,
+    MatCheckboxModule,
   ],
   templateUrl: './statistic.component.html',
   styleUrl: './statistic.component.less'
@@ -59,10 +66,10 @@ export class StatisticComponent implements OnInit {
         display: true,
       }
     },
-    maintainAspectRatio:false,
-    responsive:true
+    maintainAspectRatio: false,
+    responsive: true
   };
-  
+
 
   public chartData: ChartData = {
     labels: [],
@@ -74,33 +81,43 @@ export class StatisticComponent implements OnInit {
   };
 
   public chartType: any = 'line';
-  dayOptions: { label: string, value: number }[] = [
-    { label: 'Monday', value: 1 },
-    { label: 'Tuesday', value: 2 },
-    { label: 'Wednesday', value: 3 },
-    { label: 'Thursday', value: 4 },
-    { label: 'Friday', value: 5 },
+  dayOptions: { label: string, value: number , checked: boolean}[] = [
+    { label: 'Monday', value: 1 , checked: false},
+    { label: 'Tuesday', value: 2 , checked: false},
+    { label: 'Wednesday', value: 3 , checked: false},
+    { label: 'Thursday', value: 4 , checked: false},
+    { label: 'Friday', value: 5 , checked: false},
   ];
 
-  monthOptions: { label: string, value: number }[] = [
-    { label: 'January', value: 1 },
-    { label: 'February', value: 2 },
-    { label: 'March', value: 3 },
-    { label: 'April', value: 4 },
-    { label: 'May', value: 5 },
-    { label: 'June', value: 6 },
-    { label: 'July', value: 7 },
-    { label: 'August', value: 8 },
-    { label: 'September', value: 9 },
-    { label: 'October', value: 10 },
-    { label: 'November', value: 11 },
-    { label: 'December', value: 12 },
+  monthOptions: { label: string, value: number , checked: boolean}[] = [
+    { label: 'January', value: 1 , checked: false},
+    { label: 'February', value: 2 , checked: false},
+    { label: 'March', value: 3 , checked: false},
+    { label: 'April', value: 4 , checked: false},
+    { label: 'May', value: 5 , checked: false},
+    { label: 'June', value: 6 , checked: false},
+    { label: 'July', value: 7 , checked: false},
+    { label: 'August', value: 8 , checked: false},
+    { label: 'September', value: 9 , checked: false},
+    { label: 'October', value: 10 , checked: false},
+    { label: 'November', value: 11 , checked: false},
+    { label: 'December', value: 12 , checked: false},
   ];
 
   rooms: any[] = [];
   semesters: any[] = [];
-  statOptions: { roomPicker?: boolean, singleRoomPicker?: boolean, daypicker?: boolean, label: string, value: string, semesterPicker?: boolean; monthPicker?: boolean , datePicker?: boolean,}[] = [];
-  req = {
+  statOptions: { roomPicker?: boolean, singleRoomPicker?: boolean, daypicker?: boolean, label: string, value: string, semesterPicker?: boolean; monthPicker?: boolean, datePicker?: boolean, }[] = [];
+  req: {
+    days: any[];
+    months: any[];
+    semesterIds: any[];
+    roomIds: any[];
+    singleRoomId: any;
+    dateRange: {
+      start: any;
+      end: any;
+    };
+  } = {
     days: [],
     months: [],
     semesterIds: [],
@@ -110,19 +127,19 @@ export class StatisticComponent implements OnInit {
       start: '',
       end: ''
     }
-  }
+  };
   datasets: { data: any; backgroundColor: any; label: any; borderColor: any; }[] = []
   frequencyData: any[] = [];
   percentageData: any[] = [];
   toggleFrequencyFlag: boolean = false;
 
   selectedAction = ''
-  roomPicker = false;
-  singleRoomPicker = false;
-  daypicker = false;
-  monthPicker = false;
-  semesterPicker = false;
-  datePicker = false;
+  public roomPicker = false;
+  public singleRoomPicker = false;
+  public daypicker = false;
+  public monthPicker = false;
+  public semesterPicker = false;
+  public datePicker = false;
   constructor(private RoomsService: RoomsService, private statisticsService: StatisticsService, private generalRequestService: GeneralRequestService) {
 
   }
@@ -134,14 +151,15 @@ export class StatisticComponent implements OnInit {
     this.RoomsService.getAllRooms().subscribe((resp: any) => {
       this.rooms = resp
     })
-    console.log(this.statOptions)
+    console.log(this.daypicker)
   }
   getData() {
     this.statisticsService.callAction(this.selectedAction, this.req).subscribe((resp: any) => {
       this.udpateChart(resp)
     })
   }
-  chartOptionChange($event: MatSelectChange) {
+  chartOptionChange(action: any) {
+    console.log('hull?')
     this.roomSelect ? this.roomSelect.value = null : null;
     this.singleRoomSelect ? this.singleRoomSelect.value = null : null;
     this.daySelect ? this.daySelect.value = null : null;
@@ -158,14 +176,14 @@ export class StatisticComponent implements OnInit {
         end: ''
       }
     }
-    this.statOptions.find(option => option.value == $event.value)?.daypicker ? this.daypicker = true : this.daypicker = false;
-    this.statOptions.find(option => option.value == $event.value)?.monthPicker ? this.monthPicker = true : this.monthPicker = false;
-    this.statOptions.find(option => option.value == $event.value)?.semesterPicker ? this.semesterPicker = true : this.semesterPicker = false;
-    this.statOptions.find(option => option.value == $event.value)?.roomPicker ? this.roomPicker = true : this.roomPicker = false;
-    this.statOptions.find(option => option.value == $event.value)?.singleRoomPicker ? this.singleRoomPicker = true : this.singleRoomPicker = false;
-    this.statOptions.find(option => option.value == $event.value)?.datePicker ? this.datePicker = true : this.datePicker = false;
+    this.statOptions.find(option => option.value == action)?.daypicker ? this.daypicker = true : this.daypicker = false;
+    this.statOptions.find(option => option.value == action)?.monthPicker ? this.monthPicker = true : this.monthPicker = false;
+    this.statOptions.find(option => option.value == action)?.semesterPicker ? this.semesterPicker = true : this.semesterPicker = false;
+    this.statOptions.find(option => option.value == action)?.roomPicker ? this.roomPicker = true : this.roomPicker = false;
+    this.statOptions.find(option => option.value == action)?.singleRoomPicker ? this.singleRoomPicker = true : this.singleRoomPicker = false;
+    this.statOptions.find(option => option.value == action)?.datePicker ? this.datePicker = true : this.datePicker = false;
 
-    this.selectedAction = $event.value
+    this.selectedAction = action
     this.getData()
   }
   dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
@@ -175,29 +193,66 @@ export class StatisticComponent implements OnInit {
     }
     this.getData()
   }
-  roomOptionChange($event: MatSelectChange) {
-    this.req.roomIds = []
-    this.req.roomIds = $event.value
+  roomOptionChange($event: any, room: any) {
+    $event.stopPropagation();
+    const index = this.req.roomIds.indexOf(room.id);
+    if (index === -1) {
+      // If room_id is not in the array, add it
+      this.req.roomIds.push(room.id);
+      room.checked = true;
+    } else {
+      // If room_id is already in the array, remove it
+      this.req.roomIds.splice(index, 1);
+      room.checked = false;
+    }
     this.getData()
   }
-  singleRoomOptionChange($event: MatSelectChange) {
+  singleRoomOptionChange($event: any, roomId: any) {    
+    $event.stopPropagation();
     this.req.singleRoomId = ''
-    this.req.singleRoomId = $event.value
+    this.req.singleRoomId = roomId;
     this.getData()
   }
-  dayOptionChange($event: MatSelectChange) {
-    this.req.days = []
-    this.req.days = $event.value
+  dayOptionChange($event: any, day: any) {
+    $event.stopPropagation();
+    const index = this.req.days.indexOf(day.value);
+    if (index === -1) {
+      // If room_id is not in the array, add it
+      this.req.days.push(day.value);
+      day.checked = true;
+    } else {
+      // If room_id is already in the array, remove it
+      this.req.days.splice(index, 1);
+      day.checked = false;
+    }
     this.getData()
   }
-  monthOptionChange($event: MatSelectChange) {
-    this.req.months = []
-    this.req.months = $event.value
+  monthOptionChange($event: any, month: any) {
+    $event.stopPropagation();
+    const index = this.req.months.indexOf(month.value);
+    if (index === -1) {
+      // If room_id is not in the array, add it
+      this.req.months.push(month.value);
+      month.checked = true;
+    } else {
+      // If room_id is already in the array, remove it
+      this.req.months.splice(index, 1);
+      month.checked = false;
+    }
     this.getData()
   }
-  semesterOptionChange($event: MatSelectChange) {
-    this.req.semesterIds = []
-    this.req.semesterIds = $event.value
+  semesterOptionChange($event: any, semester: any) {
+    $event.stopPropagation();
+    const index = this.req.semesterIds.indexOf(semester.id);
+    if (index === -1) {
+      // If room_id is not in the array, add it
+      this.req.semesterIds.push(semester.id);
+      semester.checked = true;
+    } else {
+      // If room_id is already in the array, remove it
+      this.req.semesterIds.splice(index, 1);
+      semester.checked = false;
+    }
     this.getData()
   }
   udpateChart(data: any) {
@@ -224,15 +279,13 @@ export class StatisticComponent implements OnInit {
           backgroundColor: this.rooms[element.room_id - 1].color,
           borderColor: this.rooms[element.room_id - 1].color,
           label: element.options.label
-        }        
+        }
         this.frequencyData[i] = element.data.frequency;
         this.percentageData[i] = element.data.percentage;
         i++;
       })
       this.chartData.datasets = this.datasets;
     }
-    console.log(this.frequencyData)
-    console.log(this.percentageData)
     this.chartData.labels = data[0].data.labels
     this.chartType = data[0].options.chartType;
     this.barChart?.update();
@@ -240,7 +293,7 @@ export class StatisticComponent implements OnInit {
   }
   toggleFrequency() {
     let i = 0;
-    if(this.toggleFrequencyFlag){
+    if (this.toggleFrequencyFlag) {
       this.frequencyData.forEach((element: any) => {
         this.datasets[i] = {
           ...this.datasets[i],
@@ -258,8 +311,8 @@ export class StatisticComponent implements OnInit {
         this.chartData.datasets = this.datasets;
         i++;
       })
-    }    
+    }
     this.barChart?.update();
-    this.toggleFrequencyFlag = !this.toggleFrequencyFlag;   
+    this.toggleFrequencyFlag = !this.toggleFrequencyFlag;
   }
 }
