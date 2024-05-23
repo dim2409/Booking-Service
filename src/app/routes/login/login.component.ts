1079a679b746600124e7ef6b53cbd1c17c7f21ae
Ioatsi <giannischil@gmail.com>
2024-05-23 14:47:25 +0300

    adjust

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
@@ -17,10 +18,22 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private route: ActivatedRoute) { }
   login() {
     this.authenticationService.login(this.username, this.password).subscribe(response => {
       console.log('logged in')
+    });
+  }
+  ngOnInit(): void {
+    // Extract ticket parameter from the URL
+    this.route.queryParams.subscribe(params => {
+      const ticket = params['ticket'];
+      if (ticket) {
+        // Validate the ticket with the CAS server
+        this.authenticationService.validateCASTicket(ticket).subscribe(response => {
+          // Once validated, handle user authentication and redirect to protected route
+        });
+      }
     });
   }
 }
--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,21 @@ export class AuthenticationService {
 
   login(username: string, password: string) {
     return this.http.post<any>(environment.apiUrl + '/login', { username, password });
+  }
+  generateCASLoginUrl(): string {
+    // CAS server base URL
+    const casBaseUrl = 'https://sso.ihu.gr';
+    
+    // URL of your Angular application's CAS callback endpoint
+    const serviceUrl = encodeURIComponent('http://booking.iee.ihu.gr/cas/callback');
+
+    // Construct the CAS login URL
+    return `${casBaseUrl}/login?service=${serviceUrl}`;
+  }
+
+  validateCASTicket(ticket: string): Observable<any> {
+    // Send a validation request to the CAS server to validate the ticket
+    return this.http.get<any>(`https://sso.ihu.gr/serviceValidate?ticket=${ticket}`);
   }
 
 }
