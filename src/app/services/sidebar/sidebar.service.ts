82915d13f672398031d5bdb646754143c5d881c2
Ioannis Tsilikas <tsilikas@arx.gr>
2024-06-03 13:34:16 +0300

    adj

--- src/app/services/sidebar/sidebar.service.ts
+++ src/app/services/sidebar/sidebar.service.ts
@@ -67,17 +67,17 @@ export class SidebarService {
   };
   getMainSidebarItems() {
     let items = [];
-    let userRoles = this.authService.getUserRoles();
-
+    let userRoles = this.authService.getUserRoles().map((role: { name: any; }) => role.name);
+  
     for (let key in this.mainItems) {
       const item = this.mainItems[key];
       const filters = item.filter;
-
-      if (filters.includes('all') || filters.some((role: string) => userRoles.includes(role))) {
+  
+      if (filters.includes('all') || filters.some((role: any) => userRoles.includes(role))) {
         items.push(item.data);
       }
     }
-
+  
     return items;
   }
   toggle() {
